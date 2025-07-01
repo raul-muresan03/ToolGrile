03a9f08e0c6b5294590f6abce90c230155d53cea
Raul <raulmuresancalin@gmail.com>
2025-07-02 02:41:04 +0300

    mvp (final) - working bboxes but detects fingers too

---
+++ pdf2image.py
@@ -0,0 +1,15 @@
+import fitz
+import os
+
+in_path = "input_pdf/culegere_chimie_cluj.pdf"
+out_path = "pdf2images"
+os.makedirs(out_path, exist_ok=True)
+
+doc = fitz.open(in_path)
+for page_num in range(len(doc)):
+    page = doc.load_page(page_num)
+    pix = page.get_pixmap(dpi=300)
+    filename = f"pagina_{page_num + 1}.png"
+    pix.save(os.path.join(out_path, filename), "PNG")
+
+print("Imaginile au fost salvate in folderul:", out_path)
